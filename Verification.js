1c8f995fa46ce48a58ac00f28b8ba960cbc3f4c9
Python C# mix <138079589+Lawrence123454321@users.noreply.github.com>
2025-09-12 04:17:23 +0000

    Added new file

--- Verification.js
+++ Verification.js
@@ -4,10 +4,21 @@ let p=document.getElementById("Test");
 const urlParams = new URLSearchParams(window.location.search);
 const param = urlParams.get('next');
 function oc() {
-    if(i.value=="GAC05657") {
+    let a=generate(8);
+    console.log(a);
+    if(i.value==a) {
         p.innerHTML="Redirecting to "+param+"...";
         window.location.href=param;
     }
     alert("Wrong Passkey!");
+}
+function generate(length) {
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 s.addEventListener("click", oc);
