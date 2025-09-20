b6d891d0789e899bd89e299a04e349b8dcb96d9e
Python C# mix <138079589+Lawrence123454321@users.noreply.github.com>
2025-09-20 03:52:10 +0000

    Fixed few things

--- login/PutData.js
+++ login/PutData.js
@@ -3,8 +3,8 @@ function save(){
     let data = "Someone wrote here.\n";
 
     writeFile('data.txt', data, (err) => {
-    if (err) throw err;
-    console.log('Data written to file');
+        if (err) throw err;
+        console.log('Data written to file');
     });
 }
 document.getElementById("save").addEventListener("click", save);
