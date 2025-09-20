9073cd3f5244aac21882e3aeced0c7bcbf1c1bbf
Python C# mix <138079589+Lawrence123454321@users.noreply.github.com>
2025-09-20 03:47:39 +0000

    Added putdata

---
+++ login/PutData.js
@@ -0,0 +1,10 @@
+import { writeFile } from 'fs';
+function save(){
+    let data = "Someone wrote here.\n";
+
+    writeFile('data.txt', data, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+    });
+}
+document.getElementById("save").addEventListener("click", save);
