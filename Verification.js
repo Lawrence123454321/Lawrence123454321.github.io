2c4c4603b30fc31673a90e10d35eb99fedb45f12
Python C# mix <138079589+Lawrence123454321@users.noreply.github.com>
2025-09-20 10:54:47 +0000

    Added SHA256 Verification

--- Verification.js
+++ Verification.js
@@ -3,28 +3,19 @@ let s=document.getElementById("submit");
 let p=document.getElementById("Test");
 const urlParams = new URLSearchParams(window.location.search);
 const param = urlParams.get('next');
-let a=int_t("Hello Hecker");
-function oc() {
-    if(i.value==a) {
-        p.innerHTML="Redirecting to "+param+"...";
+async function oc() {
+    const hash = await hashString(i.value);
+    if(hash=="d85596b1f655ade7eeccbb8ea9af70cb7f7ed83e7156afc13a1b9ce394788eb3"){
+        alert("Yay");
         window.location.href=param;
     }
-    alert("Wrong Passkey! Passkey is "+a);
-    
+    else alert("No");
 }
-async function hashSHA256(message) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
-}
-function int_t(message) {
-    let conversion = "";
-    message.forEach(element => {
-        conversion += element.charCodeAt(0);
-    });
-    return conversion;
+async function hashString(message) {
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('')
+    return hashHex;
 }
 s.addEventListener("click", oc);
